76f56ecc91712476f9650079e9a124af04cc8cc1
Hamza Basrai <hamza.basrai@gmail.com>
2020-09-18 17:42:11 -0400

    Update Babel and Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,55 +6,53 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const outputDirectory = 'dist';
 
-module.exports = (env) => {
-  return {
-    entry: path.join(__dirname, 'src', 'index.js'),
-    output: {
-      path: path.resolve(__dirname, outputDirectory),
-      filename: '[name].bundle.js',
-      chunkFilename: '[id].js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, outputDirectory),
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          use: {
-            loader: 'url-loader',
-          },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'url-loader',
         },
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-        },
-      ],
-    },
-    devServer: {
-      contentBase: path.resolve(__dirname, outputDirectory),
-      compress: true,
-      open: true,
-      clientLogLevel: 'silent',
-      historyApiFallback: true,
-      hot: true,
-      port: 3000,
-    },
-    devtool: env.NODE_ENV === 'development' ? 'source-map' : false,
-    plugins: [
-      new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin({
-        filename: '[name].bundle.css',
-        chunkFilename: '[id].css',
-      }),
-      new HtmlWebPackPlugin({
-        template: './public/index.html',
-      }),
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
     ],
-  };
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, outputDirectory),
+    compress: true,
+    open: true,
+    clientLogLevel: 'silent',
+    historyApiFallback: true,
+    hot: true,
+    port: 3000,
+  },
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
