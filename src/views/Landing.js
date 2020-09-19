0fb1ace9c1e81111137e9efc067611372ba2f956
Hamza Basrai <hamza.basrai@gmail.com>
2020-09-19 13:41:53 -0400

    Create Landing view

---
+++ src/views/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Box from '../assets/box.svg';
+import Truck from '../assets/truck.svg';
+
+const Landing = () => {
+  return (
+    <Layout>
+      <div className="h-full flex flex-col lg:flex-row lg:justify-evenly lg:p-40">
+        <button className="bg-blue-600 hover:bg-blue-700 rounded-lg m-5 flex-grow max-w-4xl transform hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out ">
+          <div className="flex flex-col h-full justify-center">
+            <Box className="mx-auto w-32 h-32 md:w-48 md:h-48 2xl:w-64 2xl:h-64"></Box>
+            <h1 className="font-bold text-white text-5xl 2xl:text-6xl">Receiving</h1>
+          </div>
+        </button>
+        <button className="bg-green-500 hover:bg-green-600 rounded-lg m-5 flex-grow max-w-4xl transform hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out ">
+          <div className="flex flex-col h-full justify-center">
+            <Truck className="mx-auto w-32 h-32 md:w-48 md:h-48 2xl:w-64 2xl:h-64"></Truck>
+            <h1 className="font-bold text-white text-5xl 2xl:text-6xl">Shipping</h1>
+          </div>
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Landing;
