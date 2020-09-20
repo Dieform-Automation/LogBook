fdb6b86eef6c27918bb9123942dcf9bb551ac8c5
Hamza Basrai <hamza.basrai@gmail.com>
2020-09-19 21:06:26 -0400

    Update icon names

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Box from '../assets/box.svg';
-import Truck from '../assets/truck.svg';
+import BoxIcon from '../assets/box.svg';
+import TruckIcon from '../assets/truck.svg';
 
 const Landing = () => {
   return (
@@ -9,13 +9,13 @@ const Landing = () => {
       <div className="h-full flex flex-col lg:flex-row lg:justify-evenly lg:p-40">
         <button className="bg-blue-600 hover:bg-blue-700 rounded-lg m-5 flex-grow max-w-4xl transform hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out ">
           <div className="flex flex-col h-full justify-center">
-            <Box className="mx-auto w-32 h-32 md:w-48 md:h-48 2xl:w-64 2xl:h-64"></Box>
+            <BoxIcon className="mx-auto w-32 h-32 md:w-48 md:h-48 2xl:w-64 2xl:h-64"></BoxIcon>
             <h1 className="font-bold text-white text-5xl 2xl:text-6xl">Receiving</h1>
           </div>
         </button>
         <button className="bg-green-500 hover:bg-green-600 rounded-lg m-5 flex-grow max-w-4xl transform hover:-translate-y-1 hover:scale-110 transition duration-300 ease-in-out ">
           <div className="flex flex-col h-full justify-center">
-            <Truck className="mx-auto w-32 h-32 md:w-48 md:h-48 2xl:w-64 2xl:h-64"></Truck>
+            <TruckIcon className="mx-auto w-32 h-32 md:w-48 md:h-48 2xl:w-64 2xl:h-64"></TruckIcon>
             <h1 className="font-bold text-white text-5xl 2xl:text-6xl">Shipping</h1>
           </div>
         </button>
