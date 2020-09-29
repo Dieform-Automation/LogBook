e98225791fb479b0c67f31d2528d7feaf02eb91e
Hamza Basrai <hamza.basrai@gmail.com>
2020-09-29 14:59:35 -0400

    Add routing from landing page to receiving page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Landing from './views/Landing';
+import ReceivingForm from './components/ReceivingForm';
 
 const App = () => {
-  return <Landing></Landing>;
+  return (
+    <Switch>
+      <Route path="/receiving" component={ReceivingForm} />
+      <Route path="/" component={Landing} />
+      <Route path="*" render={() => <Redirect to="/" />} />
+    </Switch>
+  );
 };
 
 export default App;
