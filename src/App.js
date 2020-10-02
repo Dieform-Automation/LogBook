7d8f96de2184278463c72e8780f1c951c7568e0f
Hamza Basrai <hamza.basrai@gmail.com>
2020-10-02 13:47:44 -0400

    Add /shipping route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Landing from './views/Landing';
 import ReceivingForm from './components/ReceivingForm';
+import ShippingForm from './components/ShippingForm';
 
 const App = () => {
   return (
     <Switch>
       <Route path="/receiving" component={ReceivingForm} />
+      <Route path="/shipping" component={ShippingForm} />
       <Route path="/" component={Landing} />
       <Route path="*" render={() => <Redirect to="/" />} />
     </Switch>
