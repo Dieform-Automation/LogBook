d6dac13b5a339da8d03f134e9af8be92e22fe4a3
Hamza Basrai <hamza.basrai@gmail.com>
2020-10-05 22:50:19 -0400

    Update Layout CSS classes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+      <main className="h-screen bg-gray-100">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{children}</div>
       </main>
     </>
   );
