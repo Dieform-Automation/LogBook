12aefa9c5eaaf1fe01538c1896434fb7a9222ebe
Hamza Basrai <hamza.basrai@gmail.com>
2020-10-05 23:57:16 -0400

    Rearrange Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,8 +5,8 @@ import Navbar from './Navbar';
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
       <main className="h-screen bg-gray-100">
+        <Navbar />
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{children}</div>
       </main>
     </>
