399e2c3a83b64c665d1a3b32da3fbcb42555dcea
Hamza Basrai <hamza.basrai@gmail.com>
2020-10-17 01:03:11 -0400

    Create useCustomers query

--- src/views/Customers.js
+++ src/views/Customers.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from '../components/Header';
+import useCustomers from '../hooks/useCustomers';
 
 const Customers = () => {
+  const { data, status, error } = useCustomers();
+
+  if (status === 'loading') return <div>Loading...</div>;
+  if (status === 'error') return <div>Error {error.message}</div>;
+
   return (
     <div>
       <Header title="Customers" />
+      {data.map((customer) => (
+        <h1 key={customer.id}>{customer.name}</h1>
+      ))}
     </div>
   );
 };
