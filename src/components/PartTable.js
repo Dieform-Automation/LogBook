7a7498254b643724c8f624903e1ed0731fe57f03
Hamza Basrai <hamza.basrai@gmail.com>
2020-11-01 17:43:01 -0500

    Refactor use of queries

--- src/components/PartTable.js
+++ src/components/PartTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import Select from 'react-select';
 import PropTypes from 'prop-types';
@@ -9,16 +9,26 @@ import useGlobalState from '../hooks/useGlobalState';
 import TrashIcon from '../assets/trash.svg';
 
 const PartTable = ({ customerId }) => {
-  const { receivedParts, addRecPart, removeRecPart } = useGlobalState((state) => ({
-    ...state,
-  }));
-  const { data: parts, status, error } = useParts(customerId);
+  const [receivedParts, addRecPart, removeRecPart] = useGlobalState((state) => [
+    state.receivedParts,
+    state.addRecPart,
+    state.removeRecPart,
+  ]);
+
+  const partsQuery = useParts(customerId);
   const isShipping = useRouteMatch('/shipping');
 
   const [formError, setFormError] = useState('');
   const [selectedPart, setSelectedPart] = useState();
   const quantity = useField('text');
   const bins = useField('text');
+
+  useEffect(() => {
+    setSelectedPart(null);
+    setFormError('');
+    quantity.reset();
+    bins.reset();
+  }, [customerId]);
 
   const isValidPart = () => {
     let message = '';
@@ -60,99 +70,104 @@ const PartTable = ({ customerId }) => {
     }
   };
 
-  if (status === 'loading') return <div>Loading...</div>;
-  if (status === 'error') return <div>Error {error.message}</div>;
-
-  const options = parts.map((part) => ({
-    value: part.id,
-    label: part.number,
-  }));
-
   return (
-    <div>
-      {/* Inline Fields & Button */}
-      <div className="flex flex-wrap -mx-3 mb-4 items-end">
-        <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
-          <label className="form-label" htmlFor="part-number">
-            Part Number
-          </label>
-          <Select className="shadow" options={options} onChange={setSelectedPart} />
-        </div>
-        <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
-          <label className="form-label" htmlFor="quantity">
-            Quantity
-          </label>
-          <input
-            className="form-input"
-            name="quantity"
-            id="quantity"
-            {...quantity.fields}
-            placeholder="120"
-          />
-        </div>
-        <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
-          <label className="form-label" htmlFor="bins">
-            Number of Bins
-          </label>
-          <input
-            className="form-input"
-            name="bins"
-            id="bins"
-            {...bins.fields}
-            placeholder="2"
-          />
-        </div>
-        <div className="w-full md:w-auto px-3">
-          <button
-            className={`${
-              isShipping ? 'btn-green' : 'btn-blue'
-            } btn uppercase font-bold w-full max-w-screen-md whitespace-no-wrap`}
-            type="button"
-            onClick={addPartToTable}
-          >
-            Add Part
-          </button>
-        </div>
-      </div>
-      {/* Error Message */}
-      <p className="text-center text-red-500 font-semibold text-sm my-3">{formError}</p>
-      {/* Part Table */}
-      <div className="shadow overflow-x-auto border border-gray-400 rounded mb-6">
-        <table className="min-w-full divide-y divide-gray-400">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
+    <>
+      {partsQuery.isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <div>
+          {/* Inline Fields & Button */}
+          <div className="flex flex-wrap -mx-3 mb-4 items-end">
+            <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
+              <label className="form-label" htmlFor="part-number">
                 Part Number
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
+              </label>
+              <Select
+                className="shadow"
+                value={selectedPart}
+                options={partsQuery.data.map((p) => ({ value: p.id, label: p.number }))}
+                onChange={setSelectedPart}
+              />
+            </div>
+            <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
+              <label className="form-label" htmlFor="quantity">
                 Quantity
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
-                Bins
-              </th>
-              <th className="py-3 w-6"></th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {receivedParts.map((part, key) => {
-              return (
-                <tr key={key}>
-                  <td className="px-6 py-3 whitespace-no-wrap">{part.partNumber}</td>
-                  <td className="px-6 py-3 whitespace-no-wrap">{part.quantity}</td>
-                  <td className="px-6 py-3 whitespace-no-wrap">{part.bins}</td>
-                  <td className="px-6 py-3 whitespace-no-wrap">
-                    <TrashIcon
-                      className="w-6 h-6 cursor-pointer text-red-600"
-                      onClick={() => removeRecPart(key)}
-                    />
-                  </td>
+              </label>
+              <input
+                className="form-input"
+                name="quantity"
+                id="quantity"
+                {...quantity.fields}
+                placeholder="120"
+              />
+            </div>
+            <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
+              <label className="form-label" htmlFor="bins">
+                Number of Bins
+              </label>
+              <input
+                className="form-input"
+                name="bins"
+                id="bins"
+                {...bins.fields}
+                placeholder="2"
+              />
+            </div>
+            <div className="w-full md:w-auto px-3">
+              <button
+                className={`${
+                  isShipping ? 'btn-green' : 'btn-blue'
+                } btn uppercase font-bold w-full max-w-screen-md whitespace-no-wrap`}
+                type="button"
+                onClick={addPartToTable}
+              >
+                Add Part
+              </button>
+            </div>
+          </div>
+          {/* Error Message */}
+          <p className="text-center text-red-500 font-semibold text-sm my-3">
+            {formError}
+          </p>
+          {/* Part Table */}
+          <div className="shadow overflow-x-auto border border-gray-400 rounded mb-6">
+            <table className="min-w-full divide-y divide-gray-400">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
+                    Part Number
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
+                    Quantity
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
+                    Bins
+                  </th>
+                  <th className="py-3 w-6"></th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {receivedParts.map((part, key) => {
+                  return (
+                    <tr key={key}>
+                      <td className="px-6 py-3 whitespace-no-wrap">{part.partNumber}</td>
+                      <td className="px-6 py-3 whitespace-no-wrap">{part.quantity}</td>
+                      <td className="px-6 py-3 whitespace-no-wrap">{part.bins}</td>
+                      <td className="px-6 py-3 whitespace-no-wrap">
+                        <TrashIcon
+                          className="w-6 h-6 cursor-pointer text-red-600"
+                          onClick={() => removeRecPart(key)}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/ReceivingForm.js
+++ src/components/ReceivingForm.js
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 
 import useField from '../hooks/useField';
 import useGlobalState from '../hooks/useGlobalState';
 import useCustomers from '../hooks/useCustomers';
 
-import Header from './Header';
 import PartTable from './PartTable';
 
 const ReceivingForm = () => {
-  const [customerId, setCustomerId] = useState();
+  const [selectedCustomer, setSelectedCustomer] = useState();
   const customerPackingSlip = useField('text');
   const date = useField('date');
+
   const [receivedParts, resetRecParts] = useGlobalState((state) => [
     state.receivedParts,
     state.resetRecParts,
   ]);
-  const { data: customers, status, error } = useCustomers();
-  useEffect(() => {
-    if (customers && !customerId) {
-      setCustomerId(customers[0].id);
-    }
-  }, [customers]);
+
+  const customersQuery = useCustomers();
 
   const onSubmit = (event) => {
     event.preventDefault();
     const payload = {
-      customer_id: customerId,
+      customer_id: selectedCustomer.value,
       customer_packing_slip: customerPackingSlip.fields.value,
       date: date.fields.value,
       received_parts: receivedParts.map((part) => {
@@ -40,80 +36,75 @@ const ReceivingForm = () => {
     console.log(payload);
   };
 
-  if (status === 'loading') return <div>Loading...</div>;
-  if (status === 'error') return <div>Error {error.message}</div>;
-
-  const options = customers.map((customer) => ({
-    value: customer.id,
-    label: customer.name,
-  }));
-
   return (
     <section>
-      <Header title="Receiving" />
-      <div className="shadow-md w-full max-w-3xl bg-white rounded-lg">
-        <form className="px-8 py-6" onSubmit={onSubmit}>
-          {/* Date */}
-          <div className="mb-4">
-            <label className="form-label" htmlFor="date">
-              Date
-            </label>
-            <input
-              className="form-input"
-              {...date.fields}
-              name="date"
-              id="date"
-              required
-            />
-          </div>
-          {/* Customer Select */}
-          <div className="mb-4">
-            <label className="form-label" htmlFor="customer">
-              Customer
-            </label>
-            <Select
-              className="shadow"
-              options={options}
-              onChange={(option) => {
-                let shouldChange = true;
-                if (receivedParts.length > 0) {
-                  shouldChange = confirm(
-                    'Changing customers will remove existing part entries. Are you sure?'
-                  );
-                }
-                if (shouldChange) {
-                  resetRecParts();
-                  setCustomerId(option.value);
-                }
-              }}
-            />
-          </div>
-          {/* Customer Packing Slip */}
-          <div className="mb-4">
-            <label className="form-label" htmlFor="customer-packing-slip">
-              Customer Packing Slip
-            </label>
-            <input
-              className="form-input"
-              {...customerPackingSlip.fields}
-              name="customer-packing-slip"
-              id="customer-packing-slip"
-              placeholder="17896438"
-              required
-            />
-          </div>
-          <PartTable customerId={customerId} />
-          {/* Submit Button */}
-          <div className="flex justify-center">
-            <button
-              className="btn btn-blue uppercase font-bold w-full max-w-screen-md"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
+      {customersQuery.isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <div className="shadow-md w-full max-w-3xl bg-white rounded-lg">
+          <form className="px-8 py-6" onSubmit={onSubmit}>
+            {/* Date */}
+            <div className="mb-4">
+              <label className="form-label" htmlFor="date">
+                Date
+              </label>
+              <input
+                className="form-input"
+                {...date.fields}
+                name="date"
+                id="date"
+                required
+              />
+            </div>
+            {/* Customer Select */}
+            <div className="mb-4">
+              <label className="form-label" htmlFor="customer">
+                Customer
+              </label>
+              <Select
+                className="shadow"
+                options={customersQuery.data.map((c) => ({ value: c.id, label: c.name }))}
+                onChange={(option) => {
+                  let shouldChange = true;
+                  if (receivedParts.length > 0) {
+                    shouldChange = confirm(
+                      'Changing customers will remove existing part entries. Are you sure?'
+                    );
+                  }
+                  if (shouldChange) {
+                    resetRecParts();
+                    setSelectedCustomer(option);
+                  }
+                }}
+              />
+            </div>
+            {/* Customer Packing Slip */}
+            <div className="mb-4">
+              <label className="form-label" htmlFor="customer-packing-slip">
+                Customer Packing Slip
+              </label>
+              <input
+                className="form-input"
+                {...customerPackingSlip.fields}
+                name="customer-packing-slip"
+                id="customer-packing-slip"
+                placeholder="17896438"
+                required
+              />
+            </div>
+            {selectedCustomer && <PartTable customerId={selectedCustomer.value} />}
+            {/* Submit Button */}
+            <div className="flex justify-center">
+              <button
+                className="btn btn-blue uppercase font-bold w-full max-w-screen-md"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </section>
   );
 };
