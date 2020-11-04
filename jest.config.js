674a4a060a7f5831518dd0c34f2067acb0294a15
Hamza Basrai <hamza.basrai@gmail.com>
2020-11-04 12:38:00 -0500

    Add jest html test reporter

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/fileMock.js',
   },
+  reporters: ['default', 'jest-html-reporters'],
   rootDir: 'src',
   setupFiles: ['<rootDir>/setEnvVars.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
