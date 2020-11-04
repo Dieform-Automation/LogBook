1ea53905c9ef61ee82e841808c4ccd5983599f58
Hamza Basrai <hamza.basrai@gmail.com>
2020-11-04 13:03:29 -0500

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/fileMock.js',
   },
-  reporters: ['default', 'jest-html-reporters'],
+  reporters: [
+    'default',
+    ['jest-html-reporters', { pageTitle: 'LogBook Unit Test Report' }],
+  ],
   rootDir: 'src',
   setupFiles: ['<rootDir>/setEnvVars.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
