5ff71f700d9d7cda67ef9e86327959332e49d5cd
Hamza Basrai <hamza.basrai@gmail.com>
2020-11-18 17:48:11 -0500

    Refactor PartTable to use Formik

--- src/components/PartTable.js
+++ src/components/PartTable.js
@@ -1,176 +1,108 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
-import Select from 'react-select';
 import PropTypes from 'prop-types';
+import { Form, Formik } from 'formik';
 
-import useField from '../hooks/useField';
 import useParts from '../hooks/useParts';
-import useGlobalState from '../hooks/useGlobalState';
+import useMapToOptions from '../hooks/useMapToOptions';
+import usePartTable from '../hooks/usePartTable';
+import Loader from '../elements/Loader';
+import Dropdown from '../elements/Dropdown';
+import TextInput from '../elements/TextInput';
+import Error from '../elements/Error';
 import TrashIcon from '../assets/trash.svg';
 
 const PartTable = ({ customerId }) => {
-  const [receivedParts, addRecPart, removeRecPart] = useGlobalState((state) => [
-    state.receivedParts,
-    state.addRecPart,
-    state.removeRecPart,
-  ]);
-
-  const partsQuery = useParts(customerId);
+  const { partList, addPart, removePart } = usePartTable();
+  const { data: parts, isLoading, isError } = useParts(customerId);
   const isShipping = useRouteMatch('/shipping');
 
-  const [formError, setFormError] = useState('');
-  const [selectedPart, setSelectedPart] = useState();
-  const quantity = useField('text');
-  const bins = useField('text');
+  const partOptions = React.useMemo(() => useMapToOptions(parts, 'id', 'number'), [
+    parts,
+  ]);
 
-  useEffect(() => {
-    setSelectedPart(null);
-    setFormError('');
-    quantity.reset();
-    bins.reset();
-  }, [customerId]);
-
-  const isValidPart = () => {
-    let message = '';
-
-    const existingPart = receivedParts.some((part) => {
-      return part.id === selectedPart.value;
-    });
-
-    if (!selectedPart) {
-      message = 'Part number is required';
-    } else if (existingPart) {
-      message = 'Part number already exists';
-    } else if (isNaN(quantity.fields.value) || quantity.fields.value === '') {
-      message = 'Quantity value must be a number';
-    } else if (isNaN(bins.fields.value) || bins.fields.value === '') {
-      message = 'Bins value must be a number';
-    }
-
-    if (message !== '') {
-      setFormError(message);
-      return false;
-    } else {
-      setFormError('');
-      return true;
-    }
-  };
-
-  const addPartToTable = () => {
-    if (isValidPart()) {
-      const part = {
-        id: selectedPart.value,
-        partNumber: selectedPart.label,
-        quantity: Number(quantity.fields.value),
-        bins: Number(bins.fields.value),
-      };
-      addRecPart(part);
-      quantity.reset();
-      bins.reset();
-    }
-  };
-
-  return (
+  return isLoading ? (
+    <Loader />
+  ) : isError ? (
+    <Error />
+  ) : (
     <>
-      {partsQuery.isLoading ? (
-        <span>Loading...</span>
-      ) : (
-        <div>
-          {/* Inline Fields & Button */}
-          <div className="flex flex-wrap -mx-3 mb-4 items-end">
-            <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
-              <label className="form-label" htmlFor="part-number" id="part-number">
+      <Formik
+        initialValues={{ part_id: '', quantity: '', bins: '' }}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+          addPart({
+            partNumber: partOptions.find((p) => p.value === values.part_id).label,
+            ...values,
+          });
+
+          actions.resetForm();
+        }}
+      >
+        {() => (
+          <Form>
+            <div className="flex flex-wrap -mx-3 items-end space-y-4 md:space-y-0">
+              <Dropdown
+                label="Part Number"
+                name="part_id"
+                options={partOptions}
+                resetOnChange={partList}
+                inline
+              />
+              <TextInput label="Quantity" name="quantity" inline />
+              <TextInput label="Number of Bins" name="bins" inline />
+              <div className="w-full md:w-auto px-3">
+                <button
+                  className={`${
+                    isShipping ? 'btn-green' : 'btn-blue'
+                  } btn uppercase font-bold w-full max-w-screen-md whitespace-no-wrap`}
+                  type="submit"
+                  data-testid="add-part-btn"
+                >
+                  Add Part
+                </button>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      {/* Part Table */}
+      <div className=" overflow-x-auto border border-gray-400 rounded mb-6">
+        <table className="min-w-full divide-y divide-gray-400">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
                 Part Number
-              </label>
-              <Select
-                aria-labelledby="part-number"
-                className="shadow"
-                value={selectedPart}
-                options={partsQuery.data.map((p) => ({ value: p.id, label: p.number }))}
-                onChange={setSelectedPart}
-              />
-            </div>
-            <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
-              <label className="form-label" htmlFor="quantity">
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
                 Quantity
-              </label>
-              <input
-                className="form-input"
-                name="quantity"
-                id="quantity"
-                data-testid="quantity"
-                {...quantity.fields}
-                placeholder="120"
-              />
-            </div>
-            <div className="w-full md:flex-1 px-3 mb-4 md:mb-0">
-              <label className="form-label" htmlFor="bins">
-                Number of Bins
-              </label>
-              <input
-                className="form-input"
-                name="bins"
-                id="bins"
-                data-testid="bins"
-                {...bins.fields}
-                placeholder="2"
-              />
-            </div>
-            <div className="w-full md:w-auto px-3">
-              <button
-                className={`${
-                  isShipping ? 'btn-green' : 'btn-blue'
-                } btn uppercase font-bold w-full max-w-screen-md whitespace-no-wrap`}
-                type="button"
-                data-testid="add-part-btn"
-                onClick={addPartToTable}
-              >
-                Add Part
-              </button>
-            </div>
-          </div>
-          {/* Error Message */}
-          <p className="text-center text-red-500 font-semibold text-sm my-3">
-            {formError}
-          </p>
-          {/* Part Table */}
-          <div className="shadow overflow-x-auto border border-gray-400 rounded mb-6">
-            <table className="min-w-full divide-y divide-gray-400">
-              <thead className="bg-gray-100">
-                <tr>
-                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
-                    Part Number
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
-                    Quantity
-                  </th>
-                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
-                    Bins
-                  </th>
-                  <th className="py-3 w-6"></th>
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase tracking-wide">
+                Bins
+              </th>
+              <th className="py-3 w-6"></th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {partList.map((part, key) => {
+              return (
+                <tr key={key}>
+                  <td className="px-6 py-3 whitespace-no-wrap">{part.partNumber}</td>
+                  <td className="px-6 py-3 whitespace-no-wrap">{part.quantity}</td>
+                  <td className="px-6 py-3 whitespace-no-wrap">{part.bins}</td>
+                  <td className="px-6 py-3 whitespace-no-wrap">
+                    <TrashIcon
+                      className="w-6 h-6 cursor-pointer text-red-600"
+                      onClick={() => removePart(key)}
+                    />
+                  </td>
                 </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {receivedParts.map((part, key) => {
-                  return (
-                    <tr key={key}>
-                      <td className="px-6 py-3 whitespace-no-wrap">{part.partNumber}</td>
-                      <td className="px-6 py-3 whitespace-no-wrap">{part.quantity}</td>
-                      <td className="px-6 py-3 whitespace-no-wrap">{part.bins}</td>
-                      <td className="px-6 py-3 whitespace-no-wrap">
-                        <TrashIcon
-                          className="w-6 h-6 cursor-pointer text-red-600"
-                          onClick={() => removeRecPart(key)}
-                        />
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
