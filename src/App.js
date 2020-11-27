582e36cbc93c916c2815cecf63a01a2c2f053d03
Hamza Basrai <hamza.basrai@gmail.com>
2020-11-27 14:24:43 -0500

    Create Shipping page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import Landing from './views/Landing';
-import ReceivingForm from './components/ReceivingForm';
-import ShippingForm from './components/ShippingForm';
-import Layout from './components/Layout';
-import Customers from './views/Customers';
 import { ReactQueryCacheProvider, QueryCache } from 'react-query';
 import { ToastContainer } from 'react-toastify';
+
+import ReceivingForm from './components/ReceivingForm';
+import Layout from './components/Layout';
+import Landing from './views/Landing';
+import Customers from './views/Customers';
 import Parts from './views/Parts';
+import Shipping from './views/Shipping';
 
 const queryCache = new QueryCache();
 
@@ -17,7 +18,7 @@ const App = () => {
       <Layout>
         <Switch>
           <Route path="/receiving" component={ReceivingForm} />
-          <Route path="/shipping" component={ShippingForm} />
+          <Route path="/shipping" component={Shipping} />
           <Route path="/customers" component={Customers} />
           <Route path="/parts" component={Parts} />
           <Route path="/" component={Landing} />
---
+++ src/views/Shipping.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../elements/Header';
+import ShippingForm from '../components/ShippingForm';
+
+const Shipping = () => {
+  return (
+    <>
+      <Header title="Shipping" />
+      <ShippingForm />
+    </>
+  );
+};
+
+export default Shipping;
