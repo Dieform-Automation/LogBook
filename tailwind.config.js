586b163cd058028054b72e7702a84be625972ca8
Hamza Basrai <hamza.basrai@gmail.com>
2020-12-02 13:06:41 -0500

    Add rotate animations to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
     extend: {
       animation: {
         'spin-slow': 'spin 3s linear infinite',
+        'rotate-90-cw': 'rotate-90-cw 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'rotate-90-ccw':
+          'rotate-90-ccw 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
+      keyframes: {
+        'rotate-90-cw': {
+          '0%': { transform: 'rotate(0)' },
+          '100%': { transform: 'rotate(90deg)' },
+        },
+        'rotate-90-ccw': {
+          '0%': { transform: 'rotate(0)' },
+          '100%': { transform: 'rotate(-90deg)' },
+        },
       },
       screens: {
         '2xl': '1440px',
