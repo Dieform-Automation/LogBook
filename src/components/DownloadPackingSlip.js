edc5e10f7da4dc903f97d9b015d91841c85d3074
Hamza Basrai <hamza.basrai@gmail.com>
2020-12-03 18:05:39 -0500

    Refactor Download button into seperate component

---
+++ src/components/DownloadPackingSlip.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+
+import PackingSlip from '../components/PackingSlipTemplate';
+
+import Download from '../assets/download.svg';
+import File from '../assets/file.svg';
+import Cog from '../assets/cog.svg';
+import Exclamation from '../assets/exclamation.svg';
+
+const DownloadPackingSlip = ({ row }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="cursor-pointer flex space-x-4 justify-center items-center group">
+      {show ? (
+        <PDFDownloadLink
+          document={<PackingSlip shipment={row.original} />}
+          fileName={`PS-${row.original.packing_slip}.pdf`}
+        >
+          {({ loading, error }) =>
+            loading ? (
+              <button className="flex space-x-1 items-center py-1 px-2 rounded text-black">
+                <span>Loading...</span>
+                <Cog className="h-6 w-6 animate-spin-slow" />
+              </button>
+            ) : error ? (
+              <button className="flex space-x-1 items-center bg-red-500 py-1 px-2 rounded text-white">
+                <span>Error</span>
+                <Exclamation className="h-6 w-6 " />
+              </button>
+            ) : (
+              <button className="flex space-x-1 items-center bg-green-500 py-1 px-2 rounded text-white">
+                <span>Download</span>
+                <Download className="h-6 w-6" />
+              </button>
+            )
+          }
+        </PDFDownloadLink>
+      ) : (
+        <button
+          onClick={() => setShow(true)}
+          className="flex space-x-1 items-center bg-blue-500 py-1 px-2 rounded text-white"
+        >
+          <span>Generate</span>
+          <File className="h-6 w-6" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+DownloadPackingSlip.propTypes = {
+  row: PropTypes.object.isRequired,
+};
+
+export default DownloadPackingSlip;
